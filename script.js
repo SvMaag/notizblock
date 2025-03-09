4b0f5100f17ee9c9eec80dbd297fbeb65be2bcb6
SvMaag <sven.maag@gmail.com>
2025-03-09 15:52:43 +0100

    Notizfeld add

--- script.js
+++ script.js
@@ -5,17 +5,34 @@ let notes = ['banana','rasen mähen'];
 
 
 function renderNotes(){
-
 // ich muss definieren wo sie anzuzeigen sind
-let contentRef = document.getElementById('content')
-
-contentRef.innerHTML = notes
+    let contentRef = document.getElementById('content')
+    contentRef.innerHTML = "";
+    for (let index = 0; index < notes.length; index++) {
+        const note = notes[index];
+        contentRef.innerHTML += getNoteTempate(note);
+    }
 
 };
 
+function getNoteTempate(note){
+    return `<p>+ ${note}</p>`;
+}
+
 // global speichern
 
-// notizen add
+// notizen hinzufügen
+     // eingabe von user definieren
+function addNote(){
+     //eingabe auslesen
+    let noteInputRef = document.getElementById('note_input');
+    let noteInput = noteInputRef.value;
+    //eingabe den Notizen hinzufügen
+    notes.push(noteInput);
+    // eingabe anzeigen lassen
+    renderNotes();
+
+}
 
 // notizen löschen
 
