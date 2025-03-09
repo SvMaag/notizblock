fa17a03a2cd90271645f070931d13b17a862226b
SvMaag <sven.maag@gmail.com>
2025-03-09 14:56:29 +0100

    fist notiz

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // notizen anzeigen lassen
+// ich bruache notizen
+let notes = ['banana','rasen mähen'];
+//-> wann werden sie angezeigt?
+
+
+function renderNotes(){
+
+// ich muss definieren wo sie anzuzeigen sind
+let contentRef = document.getElementById('content')
+
+contentRef.innerHTML = notes
+
+};
 
 // global speichern
 
