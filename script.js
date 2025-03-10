e1ec41bedcd51151fd13fad9ac7f4d475e08d665
SvMaag <sven.maag@gmail.com>
2025-03-10 13:11:27 +0100

    add NitesTitle

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 // notizen anzeigen lassen
 // ich bruache notizen
+let notesTitle = ['BA','Aufgabe'];
 let notes = ['banana','rasen mähen'];
 //-> wann werden sie angezeigt?
-
+let trashNotes = [];
 
 function renderNotes(){
 // ich muss definieren wo sie anzuzeigen sind
     let contentRef = document.getElementById('content')
     contentRef.innerHTML = "";
-    for (let index = 0; index < notes.length; index++) {
-        const note = notes[index];
-        contentRef.innerHTML += getNoteTempate(note);
+    for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+        contentRef.innerHTML += getNoteTempate(indexNote);
     }
 
 };
 
-function getNoteTempate(note){
-    return `<p>+ ${note}<button>X</button></p>`;
+
+
+function renderTrashNotes() {
+    // ich muss definieren wo sie anzuzeigen sind
+    let trashContentRef = document.getElementById('trashcontent');
+    trashContentRef.innerHTML = "";
+    for (let indexTrashNote = 0; indexTrashNote < trashNotes.length; indexTrashNote++) {
+        trashContentRef.innerHTML += getTrashNoteTempate(indexTrashNote);
+    }
 }
+
+
+
+function getNoteTempate(indexNote){
+    return `<p>+ title: ${notesTitle[indexNote]} -> ${notes[indexNote]} <button onclick="deleteNote(${indexNote})">X</button></p>`;
+};   
+
+
+function getTrashNoteTempate(indexTrashNote){
+    return `<p>+ ${trashNotes[indexTrashNote]}<button onclick="deleteNote(${indexTrashNote})">X</button></p>`;
+};  
 
 // global speichern
 
@@ -38,8 +56,12 @@ function addNote(){
 // notizen löschen
     //welche Notiz muss gelöscht werden
 function deleteNote(indexNote){
-    notes.splice(indexNote, 1);
-}
+    let trashNote = notes.splice(indexNote, 1)[0];
+    trashNotes.push(trashNote);
     //wann muss die Notziz gelöscht werden
+   renderNotes();
+   renderTrashNotes();
+}
+    
 
 //nozizen archivieren
